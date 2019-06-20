62c398fc0f1bd4a13301bf259e855807b69c3a48
jambari <jambari07@gmail.com>
2019-06-20 11:31:10 +0900

    perbaiki menu metadata yg hilang

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,12 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
-
+<li class="treeview">
+	<a href="#"><i class="fa fa-book"></i> <span>Metadata</span> <i class="fa fa-angle-left pull-right"></i></a>
+	<ul class="treeview-menu">
+		<li><a href='{{ backpack_url('stasiun') }}'><i class='wi wi-raindrops' style="margin-right: 0.8em;"></i> <span>Stasiun</span></a></li>
+		<li><a href='{{ backpack_url('alat') }}'><i class='wi wi-raindrops' style="margin-right: 0.8em;"></i> <span>Alat</span></a></li>
+	</ul>
+</li>
 
 <li class="treeview">
 	<a href="#"><i class="fa fa-list"></i> <span>Data Iklim</span> <i class="fa fa-angle-left pull-right"></i></a>
